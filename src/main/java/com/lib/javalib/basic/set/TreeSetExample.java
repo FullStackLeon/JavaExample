fdf55288390101f57e801379a43dc8d11d78b9ca
leon <yuejidaren@gmail.com>
2025-05-29 23:25:27 +0800

    treeset修改

--- src/main/java/com/lib/javalib/basic/set/TreeSetExample.java
+++ src/main/java/com/lib/javalib/basic/set/TreeSetExample.java
@@ -3,6 +3,7 @@ package com.lib.javalib.basic.set;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Iterator;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 @Slf4j
@@ -19,7 +20,18 @@ public class TreeSetExample {
         }
 
         treeSet.add("d");
-        treeSet.forEach(item -> log.debug("after add, set item:{}", item));
+        treeSet.forEach(item->log.debug("after add, set item:{}", item));
+
+        SortedSet<String> headSet = treeSet.headSet("d"); // 返回小于d的元素
+        for (String item : headSet) {
+            log.debug("after headSet, set item:{}", item);
+        }
+
+        treeSet.tailSet("d");
+        SortedSet<String> tailSet = treeSet.tailSet("a"); // 返回大于等于a的元素
+        for (String item : tailSet) {
+            log.debug("after tailSet, set item:{}", item);
+        }
 
         treeSet.remove("d");
         for (String item : treeSet) {
